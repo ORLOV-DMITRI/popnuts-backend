6d7fc3777924a53c4045953a2da41ce4898dc05f
Dmitri Orlov <dmitri.orlov.anatolevich@gmail.com>
2024-06-27 14:01:51 +0600

    Очистка от лишних файлов

--- routes/old/quote.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getQuote} = require("../../controllers/old/quote");
-
-///api/total/
-router.get("/", getQuote);
-
-
-module.exports = router;
